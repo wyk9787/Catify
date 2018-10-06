e74cdf7675e82b447ef30b5225986536c33bb6bb
Shuyi Qi <shuyiqi@wireless-nat-guest-inside.uiowa.edu>
2018-10-06 11:11:43 -0500

    Added breed

--- cats_data.py
+++ cats_data.py
@@ -1,15 +1,15 @@
 all_cats = []
 
 class Cat:
-    def ___init__(self, id, name, color, longitude, latitude, neutered=false,
+    def ___init__(self, id, name, color, longitude, latitude, breed, neutered=false,
             owner='None'):
         self.id = id
         self.color=color
-        self.location = [(longitude, latitude)] 
+        self.location = [(longitude, latitude)]
+        self.breed = breed
 
     def add_location_and_image(self, longitude, latitude):
         self.location.append((longitude, latitude))
 
     def get_size(self):
         return len(self.location)
-
