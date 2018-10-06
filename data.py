ace418147fd5aa3a85741a91b3b9c5e51b17d2f9
wyk9787 <wangyika@grinnell.edu>
2018-10-06 10:59:00 -0500

    Add cat class

---
+++ data.py
@@ -0,0 +1,13 @@
+class Cat:
+    def ___init__(self, id, name, color, longitude, latitude, neutered=false,
+            owner='None'):
+        self.id = id
+        self.color=color
+        self.location = [(longitude, latitude)] 
+
+    def add_location_and_image(self, longitude, latitude):
+        self.location.append((longitude, latitude))
+
+    def get_size(self):
+        return len(self.location)
+
